4b06240c9f2016282c2fb2055b9da8099b67fd32
K4STORx <71993453+K4STORx@users.noreply.github.com>
2023-03-25 20:25:48 -0400

    start

---
+++ js/commands.js
@@ -0,0 +1,85 @@
+var linkedin = "https://www.linkedin.com/in/skylarwjackson/";
+var email = 'mailto:skylarwjackson2@gmail.com';
+
+help = [
+  "<br>",
+  'whois                    about the creator',
+  'current                  current projects',  
+  'projects                 finished projects',
+  'linkedin                 open linkedin profile',
+  'email                    contact through email',
+  'resume                   resume viewer',
+  'clear                    clears terminal',
+  'credits                  site information',
+  "<br>",
+];
+
+banner = [
+  'Console in development',
+  '[■■□□□□□□□□] 20%',
+  '<br>',
+  '<span class="eyes">  ⠤⠤⠤⠤⠤⠤⢤⣄⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</span>',
+  '<span class="eyes">  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠤⠤⠶⠶⠶⠦⠤⠤⠤⠤⠤⢤⣤⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀</span>',
+  '<span class="eyes">  ⠀⠀⠀⠀⢀⠄⢂⣠⣭⣭⣕⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠤⠀⠀⠀⠤⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠉⠉⠉</span>',
+  '<span class="eyes">  ⠀⠀⢀⠜⣳⣾⡿⠛⣿⣿⣿⣦⡠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣤⣤⣤⣤⣤⣤⣤⣤⣤⣍⣀⣦⠦⠄⣀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀</span>',
+  '<span class="eyes">  ⠀⠠⣄⣽⣿⠋⠀⡰⢿⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⡿⠛⠛⡿⠿⣿⣿⣿⣿⣿⣿⣷⣶⣿⣁⣂⣤⡄⠀⠀⠀⠀⠀⠀</span>',
+  '<span class="eyes">  ⢳⣶⣼⣿⠃⠀⢀⠧⠤⢜⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⠟⠁⠀⠀⠀⡇⠀⣀⡈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠁⠐⠀⣀⠀⠀</span>',
+  '<span class="eyes">  ⠀⠙⠻⣿⠀⠀⠀⠀⠀⠀⢹⣿⣿⡝⢿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡿⠋⠀⠀⠀⠀⠠⠃⠁⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⡿⠋⠀⠀</span>',
+  '<span class="eyes">  ⠀⠀⠀⠙⡄⠀⠀⠀⠀⠀⢸⣿⣿⡃⢼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⡏⠉⠉⠻⣿⡿⠋⠀⠀⠀⠀</span>',
+  '<span class="eyes">  ⠀⠀⠀⠀⢰⠀⠀⠰⡒⠊⠻⠿⠋⠐⡼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⠀⠀⠀⠀⣿⠇⠀⠀⠀⠀⠀</span>',
+  '<span class="eyes">  ⠀⠀⠀⠀⠸⣇⡀⠀⠑⢄⠀⠀⠀⡠⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢖⠠⠤⠤⠔⠙⠻⠿⠋⠱⡑⢄⠀⢠⠟⠀⠀⠀⠀⠀⠀</span>',
+  '<span class="eyes">  ⠀⠀⠀⠀⠀⠀⠈⠉⠒⠒⠻⠶⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡄⠀⠀⠀⠀⠀⠀⠀⠀⠡⢀⡵⠃⠀⠀⠀⠀⠀⠀⠀</span>',
+  '<span class="eyes">                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠦⣀⠀⠀⠀⠀⠀⢀⣤⡟⠉⠀⠀⠀⠀⠀⠀⠀⠀</span>',
+  '█▀ █▄▀ █▄█     █ ▄▀█ █▀▀ █▄▀ █▀ █▀█ █▄ █<span class="eyes"> ⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠙⠛⠓⠒⠲⠿⢍⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</span>',
+  "▄█ █ █  █    █▄█ █▀█ █▄▄ █ █ ▄█ █▄█ █ ▀█.dev",
+  "<br>",
+  "type help for commands.",
+];
+
+whois = [
+  "<br>",
+  "owner: sky jackson",
+  "occupation: student at liberty university",
+  "degree: information technology: data networking and security",
+  "<br>"
+];
+
+current = [
+  "<br>",
+  ">input project name for more information",
+  "<br>",
+  "propertiesmove                    college capstone",
+  "winchestercpa                     accountant portfolio",
+  "yoga site                         yoga instructors website",
+  "veterinarianorganization          public newssite for vets",
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  ">input project name for more information",
+  "<br>",
+  "nothing to see here... yet.",
+  "<br>"
+];
+
+credits = [
+  "<br>",
+  `website was reworked from <a href="https://youtu.be/KtYby2QN0kQ" target="_blank" rel="noopener noreferrer">[ForrestKnight's idea]</a>`,
+  "<br>"
+];
+
+
+
+
+
+//project pages
+propertiesmove = [
+  "<br>",
+  "name: propertiesmove",
+  "type: website",
+  "description:",
+  "contribution: project teamlead",
+  'link: <a href ="https://www.propertiesmove.com/" target="_blank" rel="noopener noreferrer">[propertiesmove.com]</a>',
+  "<br>"
+]
---
+++ js/main.js
@@ -0,0 +1,124 @@
+var before = document.getElementById("before");
+var liner = document.getElementById("liner");
+var command = document.getElementById("typer"); 
+var textarea = document.getElementById("texter"); 
+var terminal = document.getElementById("terminal");
+
+var git = 0;
+var pw = false;
+let pwd = false;
+var commands = [];
+
+setTimeout(function() {
+  loopLines(banner, "", 80);
+  textarea.focus();
+}, 100);
+
+window.addEventListener("keyup", enterKey);
+
+//init
+textarea.value = "";
+command.innerHTML = textarea.value;
+
+function enterKey(e) {
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML);
+      git = commands.length;
+      addLine("welcome@guest:~$ " + command.innerHTML, "no-animation", 0);
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
+}
+
+function commander(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "help":
+      loopLines(help, "margin", 80);
+      break;
+    case "whois":
+      loopLines(whois, "margin", 80);
+      break;
+    case "current":
+      loopLines(current, "margin", 80);
+      break;
+    case "projects":
+      loopLines(projects, "margin", 80);
+      break;
+    case "credits":
+      loopLines(credits, "margin", 80);
+      break;
+    case "email":
+      addLine('opening <a href="mailto:skylarwjackson2@gmail.com">[skylarwjackson2@gmail.com]</a>...');
+      newTab(email);
+      break;
+    case "resume":
+      addLine('opening <a href="./resume/SkylarJacksonResume.pdf" target="_blank" rel="noopener noreferrer">[resume]</a>...');
+      newTab('./resume/SkylarJacksonResume.pdf');
+      break;
+    case "clear":
+      setTimeout(function() {
+        terminal.innerHTML = '<a id="before"></a>';
+        before = document.getElementById("before");
+      }, 1);
+      break;
+    case "linkedin":
+      addLine('opening <a href="https://www.linkedin.com/in/skylarwjackson/" target="_blank" rel="noopener noreferrer">[linkedin]</a>...', 0);
+      newTab(linkedin);
+      break;
+    default:
+      addLine("command not found.", 100);
+      break;
+    //project lists
+    case "propertiesmove":
+      loopLines(propertiesmove, "margin", 80);
+      break;
+  }
+}
+
+function newTab(link) {
+  setTimeout(function() {
+    window.open(link, "_blank");
+  }, 500);
+}
+
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function() {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function(item, index) {
+    addLine(item, style, index * time);
+  });
+}
